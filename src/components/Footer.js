649e784ae8146c099f0945c824ce14f66780253e
GabeVolheim <gabevolheim@gmail.com>
2024-05-30 18:15:18 -0400

    Retooled the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 const Footer = () => {
   return (
     <footer className="text-white font-bebas mt-8">
@@ -11,21 +12,37 @@ const Footer = () => {
           </p>
         </div>
         </div>
-      <div className='flex justify-evenly mx-64 gap-8'>
-        <div className='flex flex-col items-center gap-2 w-1/3'>
-          <h3 className='text-4xl'>Links</h3>
+      <div className='flex flex-col justify-evenly items-center mx-64 gap-8'>
+        {/* <div className='flex flex-col items-center gap-2 w-1/3'>
+          <h3 className='text-4xl text-slate-400'>Links</h3>
           <a className='text-2xl text-sky-400' href='/wip'>Contact</a>
           <a className='text-2xl text-sky-400' href="https://linkedin.com/in/gabe-volheim-1035141b0">LinkedIn</a>
           <a className='text-2xl text-sky-400' href='https://github.com/GabeVolheim'>Github</a>
           <a className='text-2xl text-sky-400' href='/wip'>Midnite</a>
-        </div>
+        </div> */}
         <div className='flex flex-col items-center gap-2 w-1/3'>
-          <h3 className='text-4xl'>Contact Information</h3>
-          <p className='text-2xl'>gabevolheim@gmail.com</p>
+          <h3 className='text-4xl text-cyan-400'>Contact Information</h3>
+          <div className='flex justify-center gap-8 text-cyan-300'>
+                <a href="https://linkedin.com/in/gabe-volheim-1035141b0">
+                    <FontAwesomeIcon icon={faLinkedin} size='xl'/>
+                </a>
+                <a href='https://github.com/GabeVolheim'>
+                    <FontAwesomeIcon icon = {faGithub} size='xl'/>
+                </a>
+          </div>
+          <p className='text-2xl mt-2'>gabevolheim@gmail.com</p>
           <p className='text-2xl'>706-594-4812</p>
           <p className='text-2xl'>Anderson SC</p>
         </div>
       </div>
+      {/* <div className='flex justify-center gap-8 text-cyan-400 mt-4'>
+        <a href="https://linkedin.com/in/gabe-volheim-1035141b0">
+            <FontAwesomeIcon icon={faLinkedin} size='2xl'/>
+        </a>
+        <a href='https://github.com/GabeVolheim'>
+            <FontAwesomeIcon icon = {faGithub} size='2xl'/>
+        </a>
+      </div> */}
       <p className="bg-slate-700 text-center mt-4 p-2">© 2024 Gabe Volheim</p>
     </footer>
   );
